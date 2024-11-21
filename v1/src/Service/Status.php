96a433222c9d864e0117332d75ce5d3109b093e5
Christian Shungu <christianshungu@gmail.com>
2024-11-21 21:11:37 +0100

    Debut projet user

---
+++ v1/src/Service/Status.php
@@ -0,0 +1,23 @@
+<?php 
+
+/**
+ * File Status
+ * 
+ * User: Christian SHUNGU <christianshungu@gmail.com>
+ * Date: 11.08.2024
+ * php version 8.2
+ *
+ * @category Assessment
+ * @package  SchoolManager
+ * @author   Christian SHUNGU <christianshungu@gmail.com>
+ * @license  See LICENSE file
+ * @link     https://manzowa.com
+ */
+namespace App\SchoolManager\Service
+{
+    enum Status: string
+    {
+        case Y = 'Y';
+        case N = 'N';
+    }
+}
